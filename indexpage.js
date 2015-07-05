8f5b41c22467df49c8583cd21dc6ac587f60bb5f
Jeevithesh RV <jeevithesh123@gmail.com>
2015-07-06 02:56:18 +0530

    deptpage onclick

--- indexpage.js
+++ indexpage.js
@@ -15,7 +15,8 @@
 	            $("#pag2").animate( {marginLeft:"-=1000px", opacity: 1}, 700);
               break;
         case 2:
-              $("#pag2").animate( {marginLeft:"-=100px", opacity: 0.5}, 300, function(){$("#pag2").animate( {marginLeft:"+=100px", opacity: 1}, 300); }  );					
+              $("#pag2").animate( {marginLeft:"-=100px", opacity: 0.5}, 300);
+              $("#pag2").animate( {marginLeft:"+=100px", opacity: 1}, 300);  				
 			        break;
 			 }
 			 if(page < 2)
@@ -28,7 +29,8 @@
 		  switch(page)
 			{ 
 			 case 0:
-		          $("#pag0").animate( {marginLeft:"+=100px", opacity: 0.5}, 300, function(){$("#pag0").animate( {marginLeft:"-=100px", opacity: 1}, 300); }  );  
+		          $("#pag0").animate( {marginLeft:"+=100px", opacity: 0.5}, 300);
+		          $("#pag0").animate( {marginLeft:"-=100px", opacity: 1}, 300);
 					    break;
 	     case 1:
 			        $("#pag0").animate( {marginLeft:"+=1000px", opacity: 1}, 700);
@@ -44,4 +46,17 @@
 			 if(page > 0)
 			  { page = page - 1;}
 		  });
+
+
+
+	  $("#deptpage").click( function(){
+	  			$("#pag0").animate( {marginLeft:"-=1000px", opacity: 0}, 700);
+	            $("#pag1").animate( {marginLeft:"-=1000px", opacity: 1}, 700);
+		        $("#pag2").animate( {marginLeft:"-=1000px", opacity: 0}, 700);
+			});
+	  $("#clubspage").click( function(){
+	  			$("#pag0").animate( {marginLeft:"-=2000px", opacity: 0}, 700);
+	            $("#pag1").animate( {marginLeft:"-=2000px", opacity: 0}, 700);
+		        $("#pag2").animate( {marginLeft:"-=2000px", opacity: 1}, 700);
+			});
 	});
